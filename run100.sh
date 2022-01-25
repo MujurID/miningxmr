06b2c2b1815dc75c335ee308369f1be207625eb3
MujurID <mujurdotid@gmail.com>
2022-01-25 19:47:01 +0700

    Create run100.sh

---
+++ run100.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	./xmrig -o gulf.moneroocean.stream:20008 --tls -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7 -k --donate-level=0 --randomx-1gb-pages --threads=$(($(nproc --all)*100/100))
+	sleep 5
+done
