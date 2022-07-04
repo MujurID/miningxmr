ce270dc2fe9633936de1c65f15e4f3f5256f16cd
MujurID <mujurdotid@gmail.com>
2022-07-04 22:44:42 +0700

    Update run100-solo-cpuminer-avx512.sh

--- run100-solo-cpuminer-avx512.sh
+++ run100-solo-cpuminer-avx512.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	sudo ./cpuminer-avx512 -a minotaurx -o stratum+ssl://minotaurx.mine.zergpool.com:17019 -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7 -p c=XMR,m=solo,refcode=8ecb895f49c9848c8c81d025ee56c02a --cpu-threads $(($(nproc --all)*100/100))
+	sudo ./cpuminer-avx512 -a minotaurx -o stratum+tcps://minotaurx.mine.zergpool.com:17019 -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7 -p c=XMR,m=solo,refcode=8ecb895f49c9848c8c81d025ee56c02a -t $(($(nproc --all)*100/100))
 	sleep 5
 done
