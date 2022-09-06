23dbb1e7295e2f82d1793dff01e9e0fb44e8e588
MujurID <mujurdotid@gmail.com>
2022-09-06 23:44:34 +0700

    Update run100.sh

--- run100.sh
+++ run100.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	sudo ./xmrig -o gulf.moneroocean.stream:20032 --tls --donate-level=0 -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7 -t $(($(nproc --all)*100/100)) --cpu-priority=5 --huge-pages-jit --asm=auto --randomx-mode=auto --randomx-1gb-pages
+	sudo ./xmrig -o gulf.moneroocean.stream:20032 --tls --donate-level=0 -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7 -t $(($(nproc --all)*100/100)) --randomx-init=$(($(nproc --all)*100/100)) --cpu-no-yield --cpu-priority=5 --huge-pages-jit --asm=auto --randomx-mode=auto --randomx-1gb-pages --randomx-cache-qos --verbose
 	sleep 5
 done
