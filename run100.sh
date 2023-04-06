33c312b57ce7e9020c183b1f8ca9f8cc8f9916d4
Febrian Ardi Pangestu <mujurdotid@gmail.com>
2023-04-06 14:20:03 +0700

    Update run100.sh

--- run100.sh
+++ run100.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	sudo ./xmrig -o gulf.moneroocean.stream:443 --tls --keepalive --nicehash --dns-ipv6 --donate-level=0 --donate-over-proxy=0 --no-title --print-time=10 --health-print-time=10 -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7+128000 --threads=$(($(nproc --all)*100/100)) --randomx-init=$(($(nproc --all)*100/100)) --cpu-no-yield --av=0 --cpu-memory-pool=-1 --huge-pages-jit --asm=auto --randomx-mode=auto --randomx-1gb-pages --randomx-cache-qos --verbose
+	sudo ./xmrig -o gulf.moneroocean.stream:443 --tls --keepalive --nicehash --donate-level=0 --donate-over-proxy=0 --no-title --print-time=10 --health-print-time=10 -u 4Aj8aPvB9Te58xkV2bY54hbhLnb1oVAVkKUBb5PxfFRYJwtdmfqBZ4YQLGQiVCDaEAMRKG4nwKwjp5jVq5K9mSfm1pogTE7+128000 --threads=$(($(nproc --all)*100/100)) --randomx-init=$(($(nproc --all)*100/100)) --cpu-no-yield --av=0 --cpu-memory-pool=-1 --huge-pages-jit --asm=auto --randomx-mode=auto --randomx-1gb-pages --randomx-cache-qos --verbose
 	sleep 5
 done
